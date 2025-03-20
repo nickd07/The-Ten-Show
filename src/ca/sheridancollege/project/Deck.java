2c39342213784b958e3a7084ed49481a925e8512
nickd07 <68024085+nickd07@users.noreply.github.com>
2025-03-19 22:23:39 -0400

    Update Deck.java

--- src/ca/sheridancollege/project/Deck.java
+++ src/ca/sheridancollege/project/Deck.java
@@ -6,12 +6,12 @@ public class Deck extends GroupOfCards {
 
     public Deck(int size) {
         super(size);
-        populateDeck(); // Populate the deck upon creation
+        populateDeck(); 
     }
 
     // Populate the deck with cards
     public void populateDeck() {
-        if (getCards().isEmpty()) { // Make sure we're only populating an empty deck
+        if (getCards().isEmpty()) { 
             for (Suit suit : Suit.values()) {
                 for (int i = 2; i <= 14; i++) { // 2-10, J=11, Q=12, K=13, A=14
                     getCards().add(new TheTenShowCard(suit, i));
